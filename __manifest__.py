2625eedcfdfb5aaed1d7ec2d2acc5e2551c2373e
owlvano <owlvano@gmail.com>
2018-07-21 15:13:26 +0300

    support_ticket transfer

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website Help Desk / Support Ticket Mod",
+    'version': "1.6.0",
+    'author': "Ivan Sova / Sythil Tech",
+    'category': "Tools",
+    'support': "steven@sythiltech.com.au",
+    'summary': "A helpdesk / support ticket system for your website (Odoo 10.0)",
+    'description': "A helpdesk / support ticket system for your website but slightly modified",
+    'website':'https://www.odoo.com/apps/modules/10.0/website_support/',
+    'license':'LGPL-3',
+    'data': [
+        'views/website_support_ticket_views.xml',
+    ],
+    'demo': [],
+    'depends': ['mail','web', 'crm', 'website', 'website_support'],
+    'application': True,
+    'installable': True,
+}
