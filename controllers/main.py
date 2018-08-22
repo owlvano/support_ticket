1b43de4e140863185629f3b22a3cd7be3f9a7ec0
owlvano <owlvano@gmail.com>
2018-08-23 00:18:53 +0300

    rus i18n workaround forn now, tbd

---
+++ controllers/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import odoo.http as http
+from odoo.http import request
+
+class SupportTicketController(http.Controller):
+    @http.route('/support/subcategories/fetch', type='http', auth="public", website=True)
+    def support_subcategories_fetch(self, **kwargs):
+
+        values = {}
+	for field_name, field_value in kwargs.items():
+	    values[field_name] = field_value
+	            
+	sub_categories = request.env['website.support.ticket.subcategory'].sudo().search([('parent_category_id','=', int(values['category']) )])
+	
+	#Only return a dropdown if this category has subcategories
+	return_string = ""
+	
+	if sub_categories:
+	    return_string += "<div class=\"form-group\">\n"
+	    return_string += u"    <label class=\"col-md-3 col-sm-4 control-label\" for=\"subcategory\">Подкатегория</label>\n"
+	    return_string += "    <div class=\"col-md-7 col-sm-8\">\n"
+
+            return_string += "        <select class=\"form-control\" id=\"subcategory\" name=\"subcategory\">\n"
+            for sub_category in request.env['website.support.ticket.subcategory'].sudo().search([('parent_category_id','=', int(values['category']) )]):
+                return_string += "            <option value=\"" + str(sub_category.id) + "\">" + sub_category.name.encode("utf-8") + "</option>\n"
+
+            return_string += "        </select>\n"
+	    return_string += "    </div>\n"
+            return_string += "</div>\n"
+            
+        return return_string
