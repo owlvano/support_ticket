dc762e6ffeb226ffcfbf24c04069a4ddd8e33fd1
owlvano <owlvano@gmail.com>
2018-10-25 00:02:31 +0300

    datetime changed for date for now, query fixed

--- report/website_support_ticket_report.py
+++ report/website_support_ticket_report.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, tools
+import logging
+_logger = logging.getLogger(__name__)
 
-class WebsiteSupportTicket(models.Model):
+class WebsiteSupportTicketReport(models.Model):
 
     _name = "website.support.ticket.report"
     _description = "Website Support Ticket Report"
     _auto = False
 
-    create_date = fields.Datetime('Create Date', readonly=True)
-    close_time = fields.Datetime(string="Close Time")
     priority_id = fields.Many2one('website.support.ticket.priority', "Priority", readonly=True)
     partner_id = fields.Many2one('res.partner', "Partner", readonly=True)
     company_id = fields.Many2one('res.company', "Company", readonly=True)
@@ -20,17 +20,12 @@ class WebsiteSupportTicket(models.Model):
     created_nbr = fields.Integer('# of Created Tickets', readonly=True)
     closed_nbr = fields.Integer('# of Closed Tickets', readonly=True)
     not_closed_nbr = fields.Integer('# of Not Closed Tickets', readonly=True)
-    close_time = fields.Float('Close Time (in days)', digits=(16,1), readonly=True)
-
-    def timestamp_format(self, date):
-        return "\"%s\"::timestamp" % date
+    close_time_days = fields.Integer('Average Close Time (in days)', group_operator='avg', readonly=True)
 
     def _select(self, date_from, date_to):
         select_str = """
              SELECT
                     wst.id as id,
-                    wst.create_date as create_date,
-                    wst.close_time as close_time,
                     wst.priority_id as priority_id,
                     wst.partner_id as partner_id,
                     wst.company_id as company_id,
@@ -42,37 +37,43 @@ class WebsiteSupportTicket(models.Model):
                         %(where_created_nbr)s
                     ) as created_nbr,
                     ( SELECT 1 
-                        WHERE ( s.name='Customer Closed' or s.name='Staff Closed' ) 
+                        WHERE (s.name='Customer Closed' or s.name='Staff Closed')
                         %(where_closed_nbr)s
                     ) as closed_nbr,
                     ( SELECT 1 
-                        WHERE NOT ( s.name='Customer Closed' or s.name='Staff Closed' )
+                        WHERE NOT (s.name='Customer Closed' or s.name='Staff Closed')
                         %(where_not_closed_nbr)s
-                    ) as not_closed_nbr
-        """ % self._select_where_dict(date_from, date_to)
+                    ) as not_closed_nbr,
+                    (
+                        %(select_close_time_days)s
+                        %(where_closed_nbr)s
+                    ) as close_time_days
+        """ % self._select_dict(date_from, date_to)
         return select_str
 
-    def _select_where_dict(self, date_from, date_to):
-        where_dict = {
+    def _select_dict(self, date_from, date_to):
+        select_dict = {
                 "where_created_nbr": "",
                 "where_closed_nbr": "",
-                "where_not_closed_nbr": ""
+                "where_not_closed_nbr": "",
+                "select_close_time_days": "SELECT NULL"
             }
-        if date_from and date_to:
-            date_from = self.timestamp_format(date_from)
-            date_to = self.timestamp_format(date_to)                        
-            where_dict = {
-                "where_created_nbr": " WHERE ( wst.create_date BETWEEN %s AND %s ) " % ( date_from, date_to ),
-                "where_closed_nbr": "AND ( wst.close_time BETWEEN %s AND %s )" % ( date_from, date_to ),
-                "where_not_closed_nbr": "AND ( wst.create_date <= %s )" % ( date_to )
+        if date_from and date_to:                     
+            select_dict = {
+                "where_created_nbr": " WHERE (wst.create_date >= '%s' AND wst.create_date <= '%s') " % (date_from, date_to),
+                "where_closed_nbr": "AND (wst.close_time >= '%s' AND wst.close_time <= '%s')" % (date_from, date_to),
+                "where_not_closed_nbr": "AND (wst.create_date <= '%s')" % (date_to),
+                "select_close_time_days": """
+                    SELECT wst.close_time::timestamp::date - wst.create_date::timestamp::date
+                    WHERE (s.name='Customer Closed' or s.name='Staff Closed') 
+                """
             }
-        return where_dict
+        return select_dict
 
     def _group_by(self):
         group_by_str = """
                 GROUP BY
                     wst.id,
-                    wst.create_date,
                     wst.priority_id,
                     wst.partner_id,
                     wst.company_id,
@@ -83,16 +84,17 @@ class WebsiteSupportTicket(models.Model):
         """
         return group_by_str
 
-    def init(self):
+    def init(self, date_from=False, date_to=False):
         tools.drop_view_if_exists(self._cr, self._table)
 
-        date_from = self.env.context.get('date_from') or False
-        date_to = self.env.context.get('date_to') or False
-
-        self._cr.execute("""
+        query_string = """
             CREATE OR REPLACE VIEW %s as
             %s
             FROM website_support_ticket wst
                 left join website_support_ticket_states s on (wst.state = s.id)
             %s
-        """ % (self._table, self._select(date_from, date_to), self._group_by()))
+        """ % (self._table, self._select(date_from, date_to), self._group_by())
+        self._cr.execute(query_string)
+        _logger.debug("Query executed: ")
+        _logger.debug(query_string)
+
--- wizard/website_support_ticket_report_wizard.py
+++ wizard/website_support_ticket_report_wizard.py
@@ -1,19 +1,20 @@
 from odoo import api, fields, models
-from odoo.fields import Datetime
+from odoo.fields import Date
 
 class WebsiteSupportTicketReportWizard(models.TransientModel):
     _name = "website.support.ticket.report.wizard"
     _description = "Website Support Ticket Report Wizard"
 
-    date_from = fields.Datetime('Date From')
-    date_to = fields.Datetime('Date To')
+    date_from = fields.Date('Date From')
+    date_to = fields.Date('Date To')
 
     def format_date(self, raw_date):
-        return Datetime.from_string(raw_date).strftime("%d.%m.%Y")
+        return Date.from_string(raw_date).strftime("%d.%m.%Y")
         
     @api.multi
     def do_search(self):
         self.ensure_one()
+        self.env['website.support.ticket.report'].init(self.date_from, self.date_to)
         return {
             'type': 'ir.actions.act_window',
             'name': 'Support Ticket Report (%s - %s)' % (self.format_date(self.date_from), self.format_date(self.date_to)), 
