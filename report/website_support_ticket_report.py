bfac4a9070a672bfbe636f9072cddd66b7e0e8c2
owlvano <owlvano@gmail.com>
2018-10-25 13:42:36 +0300

    some vile query buffoonery

--- report/website_support_ticket_report.py
+++ report/website_support_ticket_report.py
@@ -9,6 +9,7 @@ class WebsiteSupportTicketReport(models.Model):
     _description = "Website Support Ticket Report"
     _auto = False
 
+    name = fields.Char("Ticket Name", readonly=True)
     priority_id = fields.Many2one('website.support.ticket.priority', "Priority", readonly=True)
     partner_id = fields.Many2one('res.partner', "Partner", readonly=True)
     company_id = fields.Many2one('res.company', "Company", readonly=True)
@@ -22,10 +23,11 @@ class WebsiteSupportTicketReport(models.Model):
     not_closed_nbr = fields.Integer('# of Not Closed Tickets', readonly=True)
     close_time_days = fields.Integer('Average Close Time (in days)', group_operator='avg', readonly=True)
 
-    def _select(self, date_from, date_to):
+    def _select(self, select_dict):
         select_str = """
              SELECT
                     wst.id as id,
+                    ('#' || wst.id || ': ' || wst.subject) as name,
                     wst.priority_id as priority_id,
                     wst.partner_id as partner_id,
                     wst.company_id as company_id,
@@ -33,53 +35,78 @@ class WebsiteSupportTicketReport(models.Model):
                     wst.sub_category_id as sub_category_id,
                     s.name as state,
                     wst.user_id as user_id,
-                    ( SELECT 1 
-                        %(where_created_nbr)s
-                    ) as created_nbr,
-                    ( SELECT 1 
-                        WHERE (s.name='Customer Closed' or s.name='Staff Closed')
-                        %(where_closed_nbr)s
-                    ) as closed_nbr,
-                    ( SELECT 1 
-                        WHERE NOT (s.name='Customer Closed' or s.name='Staff Closed')
-                        %(where_not_closed_nbr)s
-                    ) as not_closed_nbr,
-                    (
-                        %(select_close_time_days)s
-                        %(where_closed_nbr)s
-                    ) as close_time_days
-        """ % self._select_dict(date_from, date_to)
+                    %(created_nbr)s as created_nbr,
+                    %(closed_nbr)s as closed_nbr,
+                    %(not_closed_nbr)s as not_closed_nbr,
+                    %(close_time_days)s as close_time_days
+        """ % select_dict
         return select_str
 
-    def _select_dict(self, date_from, date_to):
+    def _select_dict(self, where_dict, select_argument=1):
+        basic_select = "( SELECT %s" % select_argument
+
         select_dict = {
-                "where_created_nbr": "",
-                "where_closed_nbr": "",
-                "where_not_closed_nbr": "",
-                "select_close_time_days": "SELECT NULL"
+                "created_nbr" : """
+                        %s
+                        %s
+                    )
+                    """ % (basic_select, where_dict["where_create_date_in_period"]),
+                "closed_nbr" : """
+                        %s
+                        WHERE %s
+                        %s
+                    )
+                    """ % (basic_select, where_dict["where_closed"], where_dict["where_close_date_in_period"]),
+                "not_closed_nbr" : """
+                        %s
+                        WHERE NOT %s
+                        %s
+                    )
+                    """ % (basic_select, where_dict["where_closed"], where_dict["where_create_date_before_date_to"]),
+                "close_time_days" : """
+                    ( SELECT %s
+                    )
+                    """ % where_dict["select_close_days"]
+            } 
+        
+        return select_dict
+
+    def _where_dict(self, date_from, date_to):
+        where_dict = {
+                "where_closed": "(s.name='Customer Closed' or s.name='Staff Closed')",
+                "where_create_date_in_period": "",
+                "where_close_date_in_period": "",
+                "where_create_date_before_date_to": "",
+                "select_close_days": "NULL"
             }
         if date_from and date_to:                     
-            select_dict = {
-                "where_created_nbr": " WHERE (wst.create_date >= '%s' AND wst.create_date <= '%s') " % (date_from, date_to),
-                "where_closed_nbr": "AND (wst.close_time >= '%s' AND wst.close_time <= '%s')" % (date_from, date_to),
-                "where_not_closed_nbr": "AND (wst.create_date <= '%s')" % (date_to),
-                "select_close_time_days": """
-                    SELECT wst.close_time::timestamp::date - wst.create_date::timestamp::date
-                    WHERE (s.name='Customer Closed' or s.name='Staff Closed') 
-                """
-            }
-        return select_dict
+            where_dict["where_create_date_in_period"] = " WHERE (wst.create_date >= '%s' AND wst.create_date <= '%s') " % (date_from, date_to)
+            where_dict["where_close_date_in_period"] = "AND (wst.close_date >= '%s' AND wst.close_date <= '%s')" % (date_from, date_to)
+            where_dict["where_create_date_before_date_to"] = "AND (wst.create_date <= '%s')" % (date_to)
+            where_dict["select_close_days"] = """
+                    wst.close_date::date - wst.create_date::timestamp::date
+                    WHERE %(where_closed)s %(where_close_date_in_period)s
+                """ % where_dict
+        
+        return where_dict
+
+    def _where(self, select_dict):
+        where_str = """
+            WHERE %(created_nbr)s OR %(closed_nbr)s OR %(not_closed_nbr)s
+        """ % select_dict
+        return where_str
 
     def _group_by(self):
         group_by_str = """
                 GROUP BY
                     wst.id,
+                    name,
                     wst.priority_id,
                     wst.partner_id,
                     wst.company_id,
                     wst.category,
                     wst.sub_category_id,
-                    s.name,
+                    state,
                     wst.user_id
         """
         return group_by_str
@@ -87,13 +114,17 @@ class WebsiteSupportTicketReport(models.Model):
     def init(self, date_from=False, date_to=False):
         tools.drop_view_if_exists(self._cr, self._table)
 
+        where_dict = self._where_dict(date_from, date_to)
+        select_dict = self._select_dict(where_dict)
+        condition_dict = self._select_dict(where_dict, "True")
         query_string = """
             CREATE OR REPLACE VIEW %s as
             %s
             FROM website_support_ticket wst
                 left join website_support_ticket_states s on (wst.state = s.id)
             %s
-        """ % (self._table, self._select(date_from, date_to), self._group_by())
+            %s
+        """ % (self._table, self._select(select_dict), self._where(condition_dict), self._group_by())
         self._cr.execute(query_string)
         _logger.debug("Query executed: ")
         _logger.debug(query_string)
